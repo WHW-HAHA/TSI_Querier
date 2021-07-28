9419dc083ad045bb34befc919f9fe6fc99a27b92
FastUniCorn <43483189+WHW-HAHA@users.noreply.github.com>
2021-07-28 08:50:10 +0200

    version 0.0.5

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ import setuptools
 
 setuptools.setup(
     name='TSI_Querier',
-    version='0.0.2',
-    scripts=['Python'],
+    version='0.0.5',
     keywords='',
     description='The querier designed for querying Time Series Insight data from WBL Big Data Platform.',
     long_description=open(
@@ -18,8 +17,14 @@ setuptools.setup(
     long_description_content_type = "text/markdown",
     author='LiuMao',
     author_email='hanwei_wang_94@outlook.com',
-    packages=setuptools.find_packages(),
     url = 'https://github.com/WHW-HAHA/TSI_Querier',
     license='MIT',
-    install_requires=['requests']
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    package_dir={"": "TSI_QUERIER"},
+    packages=setuptools.find_packages(where="TSI_QUERIER"),
 )
