cd3db0b5673c289b0bd94f011492c1b9f1b26b66
FastUniCorn <43483189+WHW-HAHA@users.noreply.github.com>
2021-08-11 21:59:55 +0200

    update name and readme of the package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name='TSI_Querier',
-    version='0.1.0',
+    version='0.1.1',
     keywords='',
     description='The querier designed for querying Time Series Insight data from WBL Big Data Platform.',
     long_description=open(
--- test.py
+++ test.py
@@ -3,8 +3,8 @@ from TSI_Querier.DataQuerier import TsiDataQuerier
 
 # fill in environment parameters
 if __name__ == '__main__':    
-    environment_variables = {'environment_fqdn': 'eaecab34-790f-4736-a7ad-6e26fb0c3f4e.env.timeseries.azure.com',
-                             'client_secret': '-YnQjBK_5XV~mjZgyoDl6U4j..9h0hPmD3',
+    environment_variables = {'environment_fqdn':'8cd5ba2e-8801-45a8-9ed6-3ff30b85fd0e.env.timeseries.azure.com',
+                             'client_secret':'-YnQjBK_5XV~mjZgyoDl6U4j..9h0hPmD3',
                              'client_id':'2f7a266d-a98e-468d-bf33-8336f820812e' ,
                              'tenant_id':'15f996bf-aad1-451c-8d17-9b95d025eafc' }
 
@@ -12,7 +12,8 @@ if __name__ == '__main__':
     print(querier.get_authorization_token())
     
     # fill in query parameters
-    query_variables = {'time_series_id':['OSX66::32/1/VP/rg-01/p--/321VPFT010950.PV',],
+    query_variables = {'storage_type': 'coldstore',
+                       'time_series_id':['OSX66::32/1/VP/rg-01/p--/321VPFT010950.PV',],
                        'start_time':'2020-05-05 00:00:00',
                        'end_time':'2020-05-05 02:00:00',
                        'filter':{"tsx": "($event.value.Double != null)"},
